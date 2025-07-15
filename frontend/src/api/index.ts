366307a74f100e21be874f84b60c6a5a3faf10e5
Marlon Cesar Pilonetto <marlon.pilonetto@gmail.com>
2025-07-15 14:07:56 -0300

    api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from 'axios'
 
 // Instância do Axios com baseURL do .env
 const http: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://app.ainternetgroup.com',
   timeout: 60000,
 })
 
